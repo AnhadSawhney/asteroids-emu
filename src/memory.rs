28af6a5222ca8d5011256f07eebacd397ec6c939
Paul Marshall <paulama@ftml.net>
2019-01-12 23:10:53 +0000

    Tidy memory code a bit

--- src/memory.rs
+++ src/memory.rs
@@ -1,4 +1,5 @@
 // Emulate memory and memory mapped IO for Asteroids game
+
 use find_folder;
 use std::fs::File;
 use std::io::prelude::*;
@@ -8,27 +9,27 @@ pub struct MappedIO {
     pub halt: u8,
     pub swhyper: u8,
     pub swfire: u8,
-    swdiagst: u8,
-    swslam: u8,
-    swtest: u8,
+    //swdiagst: u8,
+    //swslam: u8,
+    //swtest: u8,
     
-    swlcoin: u8,    // from 0x2400
-    swccoin: u8,
-    swrcoin: u8,
+    //swlcoin: u8,    // from 0x2400
+    //swccoin: u8,
+    //swrcoin: u8,
     pub sw1start: u8,
-    sw2start: u8,
+    //sw2start: u8,
     pub swthrust: u8,
     pub swrotrght: u8,
     pub swrotleft: u8,
 
-    swcoinage: u8,    // 0x2800
-    swcnrmult: u8,
-    swcncmult: u8,
-    swlanguage: u8,
+    //swcoinage: u8,    // 0x2800
+    //swcnrmult: u8,
+    //swcncmult: u8,
+    //swlanguage: u8,
 
     pub godvg: u8,    // 0x3000
-    lmpscns: u8,  // 0x3200
-    watchdog: u8, // 0x3400
+    //lmpscns: u8,  // 0x3200
+    //watchdog: u8, // 0x3400
     pub sndexp: u8,   // 0x3600
     pub sndthump: u8, // 0x3a00
     pub sndsaucr: u8, // from 0x3c00
@@ -47,27 +48,13 @@ impl MappedIO {
             halt: 0,
             swhyper: 0,
             swfire: 0,
-            swdiagst: 0,
-            swslam: 0,
-            swtest: 0x0,
     
-            swlcoin: 0,
-            swccoin: 0,
-            swrcoin: 0,
             sw1start: 0,
-            sw2start: 0,
             swthrust: 0,
             swrotrght: 0,
             swrotleft: 0,
 
-            swcoinage: 0,    // free play
-            swcnrmult: 0,
-            swcncmult: 0,
-            swlanguage: 0,   // english
-
             godvg: 0,
-            lmpscns: 0,
-            watchdog: 0,
             sndexp: 0,
             sndthump: 0,
             sndsaucr: 0,
@@ -108,93 +95,64 @@ impl Memory {
     }
 
     pub fn get_byte(&self, addr: u16) -> u8 {
-        let a = addr as usize & 0x7FFF;
-        if a < 0x400 {
-            self.game_ram[a]
-        }
-        else if a >= 0x4000 && a < 0x5000 {
-            self.dvg_ram[a - 0x4000]
-        }
-        else if a >= 0x5000 && a < 0x5800 {
-            self.dvg_rom[a - 0x5000]
-        }
-        else if a >= 0x6800 {
-            self.game_rom[a - 0x6800]
-        }
-        else if a == 0x2001 {
-            self.mapped_io.clck3khz
-        }
-        else if a == 0x2002 {
-            self.mapped_io.halt
-        }
-        else if a == 0x2007 {
-            self.mapped_io.swtest
-        }
-        else if a == 0x2403 {
-            self.mapped_io.sw1start
-        }
-        else if a == 0x2004 {
-            self.mapped_io.swfire
-        }
-        else if a == 0x2405 {
-            self.mapped_io.swthrust
-        }
-        else if a == 0x2406 {
-            self.mapped_io.swrotrght
-        }
-        else if a == 0x2407 {
-            self.mapped_io.swrotleft
-        }
-        else if a == 0x2003 {
-            self.mapped_io.swhyper
-        }
-        else {
-            0
+        let addr = addr as usize & 0x7FFF;
+        match addr {
+            a if a < 0x400 => self.game_ram[a],
+            a if a >= 0x4000 && a < 0x5000 => self.dvg_ram[a - 0x4000],
+            a if a >= 0x5000 && a < 0x5800 => self.dvg_rom[a - 0x5000],
+            a if a >= 0x6800 => self.game_rom[a - 0x6800],
+            0x2001 => self.mapped_io.clck3khz,
+            0x2002 => self.mapped_io.halt,
+            0x2403 => self.mapped_io.sw1start,
+            0x2004 => self.mapped_io.swfire,
+            0x2405 => self.mapped_io.swthrust,
+            0x2406 => self.mapped_io.swrotrght,
+            0x2407 => self.mapped_io.swrotleft,
+            0x2003 => self.mapped_io.swhyper,
+            _ => 0,
         }
     }
 
     pub fn set_byte(&mut self, addr: u16, byte: u8) {
-        let a = addr as usize & 0x7FFF;
-        if a < 0x400 {
-            self.game_ram[a] = byte;
-        }
-        else if a >= 0x4000 && a < 0x5000 {
-            self.dvg_ram[a - 0x4000] = byte;
-        }
-        else {
-            match a {
-                0x2001 => {
-                    self.mapped_io.clck3khz = byte;
-                },
-                0x3000 => {
-                    self.mapped_io.godvg = byte;
-                },
-                0x3600 => {
-                    self.mapped_io.sndexp = byte;
-                },
-                0x3A00 => {
-                    self.mapped_io.sndthump = byte;
-                },
-                0x3C00 => {
-                    self.mapped_io.sndsaucr = byte;
-                },
-                0x3C01 => {
-                    self.mapped_io.sndsfire = byte;
-                },
-                0x3C02 => {
-                    self.mapped_io.sndselsau = byte;
-                },
-                0x3C03 => {
-                    self.mapped_io.sndthrust = byte;
-                },
-                0x3C04 => {
-                    self.mapped_io.sndfire = byte;
-                },
-                0x3C05 => {
-                    self.mapped_io.sndbonus = byte;
-                },
-                _ => {},
-            }
+        let addr = addr as usize & 0x7FFF;
+        match addr {
+            a if a < 0x400 => {
+                self.game_ram[a] = byte;
+            },
+            a if a >= 0x4000 && a < 0x5000 => {
+                self.dvg_ram[a - 0x4000] = byte;
+            },
+            0x2001 => {
+                self.mapped_io.clck3khz = byte;
+            },
+            0x3000 => {
+                self.mapped_io.godvg = byte;
+            },
+            0x3600 => {
+                self.mapped_io.sndexp = byte;
+            },
+            0x3A00 => {
+                self.mapped_io.sndthump = byte;
+            },
+            0x3C00 => {
+                self.mapped_io.sndsaucr = byte;
+            },
+            0x3C01 => {
+                self.mapped_io.sndsfire = byte;
+            },
+            0x3C02 => {
+                self.mapped_io.sndselsau = byte;
+            },
+            0x3C03 => {
+                self.mapped_io.sndthrust = byte;
+            },
+            0x3C04 => {
+                self.mapped_io.sndfire = byte;
+            },
+            0x3C05 => {
+                self.mapped_io.sndbonus = byte;
+            },
+            _ => {},
         }
     }
 }
