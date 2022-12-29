5021b3f62785e6f4f3f9ae61297ce045f36be5ee
Anhad <sawhney.anhad@gmail.com>
2022-12-29 02:21:49 -0500

    add serial output

--- src/display.rs
+++ src/display.rs
@@ -1,21 +1,21 @@
 // Emulate Atari Digital Vector Generator / display
-
-use sdl2::pixels;
 use sdl2::gfx::primitives::DrawRenderer;
-use sdl2::video::Window;
+use sdl2::pixels;
 use sdl2::render::Canvas;
+use sdl2::video::Window;
+use serialport::prelude::*;
 
 use memory::Memory;
 
 #[derive(Debug)]
 enum Instruction {
-    VCTR,    // 0x0 - 0x9
-    LABS,    // 0xA     - only instruction to use aobsolute coords
-    HALT,    // 0xB
-    JSRL,    // 0xC
-    RTSL,    // 0xD
-    JMPL,    // 0xE
-    SVEC,    // 0xF
+    VCTR, // 0x0 - 0x9
+    LABS, // 0xA     - only instruction to use absolute coords
+    HALT, // 0xB
+    JSRL, // 0xC
+    RTSL, // 0xD
+    JMPL, // 0xE
+    SVEC, // 0xF
 }
 
 pub struct Dvg {
@@ -26,11 +26,21 @@ pub struct Dvg {
     stack: [u16; 4],
     sp: usize,
     debug_mode: bool,
+    serialoutput: bool,
 }
 
 impl Dvg {
-    pub fn new(debug_mode: bool) -> Dvg {
-        Dvg {pc: 0, x: 0, y: 0, sf: 0, stack: [0; 4], sp: 0, debug_mode}
+    pub fn new(debug_mode: bool, serialoutput: bool) -> Dvg {
+        Dvg {
+            pc: 0,
+            x: 0,
+            y: 0,
+            sf: 0,
+            stack: [0; 4],
+            sp: 0,
+            debug_mode,
+            serialoutput,
+        }
     }
 
     fn reset(&mut self) {
@@ -61,7 +71,7 @@ impl Dvg {
         }
     }
 
-    fn screen_y(y: i16, h: u32) -> i16{
+    fn screen_y(y: i16, h: u32) -> i16 {
         // we find we have to flip y
         // also, y 0 thru 95 and 928 thru 1023 are not used
         (h as i32 - ((y - 96) as i32 * h as i32 / 832)) as i16
@@ -71,27 +81,45 @@ impl Dvg {
         (x as i32 * w as i32 / 1024) as i16
     }
 
+    fn send_command(
+        &mut self,
+        x: i16,
+        y: i16,
+        z: u16,
+        canvas: &mut Canvas<Window>,
+        port: &mut Option<Box<dyn SerialPort>>,
+    ) {
+        if self.serialoutput {
+            let (w, h) = canvas.output_size().unwrap();
+
+            if let Some(port) = port {
+                port.write(&[z as u8]).ok();
+                let a = Dvg::screen_x(x, w);
+                let b = Dvg::screen_y(y, h);
+                // send a and b one byte at a time
+                port.write(&[(a & 0x00FF) as u8, (a >> 8) as u8]).ok();
+                port.write(&[(b & 0x00FF) as u8, (b >> 8) as u8]).ok();
+            }
+        }
+    }
+
     fn line(&mut self, x: i16, y: i16, z: u16, canvas: &mut Canvas<Window>) {
         if z != 0 {
-            let color = pixels::Color::RGBA(255, 255, 255, z as u8 * 17); 
-            let (w,h) = canvas.output_size().unwrap();
+            let color = pixels::Color::RGBA(255, 255, 255, z as u8 * 17);
+            let (w, h) = canvas.output_size().unwrap();
 
             if x == self.x && y == self.y {
                 // on the vector display, a single point can be extremely
                 // bright. we can't do that so we just go bigger.
-                let _ = canvas.filled_circle(
-                    Dvg::screen_x(x, w),
-                    Dvg::screen_y(y, h),
-                    2,
-                    color);
-            }
-            else {
+                let _ = canvas.filled_circle(Dvg::screen_x(x, w), Dvg::screen_y(y, h), 2, color);
+            } else {
                 let _ = canvas.line(
                     Dvg::screen_x(x, w),
                     Dvg::screen_y(y, h),
                     Dvg::screen_x(self.x, w),
                     Dvg::screen_y(self.y, h),
-                    color);
+                    color,
+                );
             }
         }
         self.x = x;
@@ -101,25 +129,34 @@ impl Dvg {
     fn shift(a: u16, s: i16) -> u16 {
         if s >= 0 {
             a >> s
-        }
-        else {
+        } else {
             a << (-s)
         }
     }
 
-    pub fn render(&mut self, memory: &mut Memory, canvas: &mut Canvas<Window>) {
+    pub fn render(
+        &mut self,
+        memory: &mut Memory,
+        canvas: &mut Canvas<Window>,
+        port: &mut Option<Box<dyn SerialPort>>,
+    ) {
         memory.mapped_io.halt = 0xFF;
         memory.mapped_io.godvg = 0;
         self.reset();
         canvas.set_draw_color(pixels::Color::RGB(0, 0, 0));
         canvas.clear();
         while memory.mapped_io.halt != 0 {
-            self.execute_instruction(memory, canvas);
+            self.execute_instruction(memory, canvas, port);
         }
         canvas.present();
     }
 
-    fn execute_instruction(&mut self, memory: &mut Memory, canvas: &mut Canvas<Window>) {
+    fn execute_instruction(
+        &mut self,
+        memory: &mut Memory,
+        canvas: &mut Canvas<Window>,
+        port: &mut Option<Box<dyn SerialPort>>,
+    ) {
         let instr_addr = self.pc;
         let op_word1 = self.load_from_pc(memory);
         let op = Dvg::instruction_from_word(op_word1);
@@ -128,12 +165,17 @@ impl Dvg {
             _ => 0,
         };
         if self.debug_mode {
-            println!("---DVG X: {}, Y: {}, SF: {}, SP: {}, PC: {}", self.x, self.y, self.sf, self.sp, self.pc);
+            println!(
+                "---DVG X: {}, Y: {}, SF: {}, SP: {}, PC: {}",
+                self.x, self.y, self.sf, self.sp, self.pc
+            );
             if op_word2 == 0 {
                 println!("---DVG {:04X} {:016b} {:?}", instr_addr, op_word1, op);
-            }
-            else {
-                println!("---DVG {:04X} {:016b} {:016b} {:?}", instr_addr, op_word1, op_word2, op);
+            } else {
+                println!(
+                    "---DVG {:04X} {:016b} {:016b} {:?}",
+                    instr_addr, op_word1, op_word2, op
+                );
             }
             println!("---DVG");
         }
@@ -145,25 +187,39 @@ impl Dvg {
                 let xs = (0x400 & op_word2) != 0;
                 let delta_x = 0x3FF & op_word2;
                 let shift_bits = 9 - ((op_word1 & 0xF000) >> 12) as i16 + self.sf;
-                let x = self.x + Dvg::shift(delta_x, shift_bits) as i16 *
-                    if xs {-1} else {1};
-                let y = self.y + Dvg::shift(delta_y, shift_bits) as i16 *
-                    if ys {-1} else {1};
+                let x = self.x + Dvg::shift(delta_x, shift_bits) as i16 * if xs { -1 } else { 1 };
+                let y = self.y + Dvg::shift(delta_y, shift_bits) as i16 * if ys { -1 } else { 1 };
                 self.line(x, y, z, canvas);
-            },
+                self.send_command(x, y, z, canvas, port);
+            }
             Instruction::LABS => {
+                // CUR
                 let ys = (0x400 & op_word1) != 0;
                 let y = 0x3FF & op_word1;
                 let xs = (0x400 & op_word2) != 0;
                 let x = 0x3FF & op_word2;
                 let sf = (op_word2 & 0xF000) >> 12;
-                self.sf = if sf & 0x8 == 0 {- (sf as i16)} else {16 - sf as i16};
-                self.y = if ys {0 - ((y ^ 0x3FF) + 1) as i16} else {y as i16};
-                self.x = if xs {0 - ((x ^ 0x3FF) + 1) as i16} else {x as i16};
-            },
+                self.sf = if sf & 0x8 == 0 {
+                    -(sf as i16)
+                } else {
+                    16 - sf as i16
+                };
+                self.y = if ys {
+                    0 - ((y ^ 0x3FF) + 1) as i16
+                } else {
+                    y as i16
+                };
+                self.x = if xs {
+                    0 - ((x ^ 0x3FF) + 1) as i16
+                } else {
+                    x as i16
+                };
+
+                self.send_command(self.x, self.y, 0, canvas, port);
+            }
             Instruction::HALT => {
                 memory.mapped_io.halt = 0;
-            },
+            }
             Instruction::JSRL => {
                 if self.sp > 3 {
                     panic!("DVG stack overflow");
@@ -172,18 +228,18 @@ impl Dvg {
                 self.stack[self.sp] = self.pc;
                 self.sp += 1;
                 self.pc = addr;
-            },
+            }
             Instruction::RTSL => {
                 if self.sp == 0 {
                     panic!("DVG stack underflow");
                 }
                 self.sp -= 1;
                 self.pc = self.stack[self.sp];
-            },
+            }
             Instruction::JMPL => {
                 let a = op_word1 & 0xFFF;
                 self.pc = a;
-            },
+            }
             Instruction::SVEC => {
                 let sf = ((op_word1 & 0x800) >> 11) + ((op_word1 & 0x8) >> 2);
                 let ys = (op_word1 & 0x400) != 0;
@@ -193,12 +249,11 @@ impl Dvg {
                 let z = (op_word1 & 0xF0) >> 4;
 
                 let shift_bits = (7 - sf as i16) + self.sf;
-                let x = self.x + Dvg::shift(delta_x, shift_bits) as i16 *
-                    if xs {-1} else {1};
-                let y = self.y + Dvg::shift(delta_y, shift_bits) as i16 *
-                    if ys {-1} else {1};
+                let x = self.x + Dvg::shift(delta_x, shift_bits) as i16 * if xs { -1 } else { 1 };
+                let y = self.y + Dvg::shift(delta_y, shift_bits) as i16 * if ys { -1 } else { 1 };
                 self.line(x, y, z, canvas);
-            },
+                self.send_command(x, y, z, canvas, port);
+            }
         };
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,27 @@
+// combine roms cat 035127-02.np3 035145-02.ef2 035144-02.h2 035143-02.j2 > asteroids.rom
+
 // Enulator to run Asteroids game
 extern crate find_folder;
 extern crate sdl2;
+extern crate serialport;
+
+use serialport::prelude::*;
 
 mod cpu;
-mod memory;
 mod display;
 mod input;
+mod memory;
 mod sound;
 
 use sdl2::event::{Event, WindowEvent};
 use sdl2::keyboard::Keycode;
+use sdl2::mixer::{Channel, AUDIO_S16LSB, DEFAULT_CHANNELS, INIT_OGG, MAX_VOLUME};
 use sdl2::video::WindowPos;
-use sdl2::mixer::{DEFAULT_CHANNELS, INIT_OGG, AUDIO_S16LSB, Channel, MAX_VOLUME};
-use std::time::{Duration, Instant};
-use std::thread::sleep;
 use std::env;
+use std::thread::sleep;
+use std::time::{Duration, Instant};
 
-const SCREEN_WIDTH: u32 = 10240;    // i.e. bigger than maximised dimensions
+const SCREEN_WIDTH: u32 = 10240; // i.e. bigger than maximised dimensions
 const NMI_CYCLES: u64 = 6000;
 const TICKS_PER_SLEEP: u32 = 20;
 
@@ -31,7 +36,8 @@ fn main() {
 
     let sdl_context = sdl2::init().unwrap();
     let video_subsys = sdl_context.video().unwrap();
-    let window = video_subsys.window("Asteroids Emu", SCREEN_WIDTH, SCREEN_WIDTH)
+    let window = video_subsys
+        .window("Asteroids Emu", SCREEN_WIDTH, SCREEN_WIDTH)
         .resizable()
         .maximized()
         .opengl()
@@ -40,11 +46,12 @@ fn main() {
 
     let _audio = sdl_context.audio().unwrap();
     sdl2::mixer::open_audio(
-        44_100, // frequency
+        44_100,       // frequency
         AUDIO_S16LSB, //format
         DEFAULT_CHANNELS,
-        1_024 // chunk size
-    ).unwrap();
+        1_024, // chunk size
+    )
+    .unwrap();
     let _mixer_context = sdl2::mixer::init(INIT_OGG).unwrap();
     Channel::all().set_volume(MAX_VOLUME / 2);
 
@@ -54,8 +61,25 @@ fn main() {
 
     let mut events = sdl_context.event_pump().unwrap();
 
+    let mut settings: SerialPortSettings = Default::default();
+    settings.timeout = Duration::from_millis(10);
+    settings.baud_rate = 115200;
+
+    let mut serialoutput = true;
+
+    // Open the serial port
+    let mut port: Option<Box<dyn SerialPort>> =
+        match serialport::open_with_settings("/dev/ttyUSB0", &settings) {
+            Ok(port) => Some(port),
+            Err(e) => {
+                println!("Error opening serial port: {}", e);
+                serialoutput = false;
+                None
+            }
+        };
+
     let mut cpu = Cpu::new(debug);
-    let mut dvg = Dvg::new(debug);
+    let mut dvg = Dvg::new(debug, serialoutput);
     let mut memory = Memory::new();
     let mut sounds = Sounds::new();
     cpu.reset(&memory);
@@ -66,47 +90,55 @@ fn main() {
         for _i in 0..TICKS_PER_SLEEP {
             for event in events.poll_iter() {
                 match event {
-                    Event::Quit {..} => break 'main,
+                    Event::Quit { .. } => break 'main,
 
-                    Event::KeyDown {keycode: Some(keycode), ..} => {
+                    Event::KeyDown {
+                        keycode: Some(keycode),
+                        ..
+                    } => {
                         if keycode == Keycode::Escape {
                             break 'main;
-                        }
-                        else {
+                        } else {
                             input::update_from_input(keycode, true, &mut memory);
                         }
-                    },
+                    }
 
-                    Event::KeyUp {keycode: Some(keycode), ..} => {
+                    Event::KeyUp {
+                        keycode: Some(keycode),
+                        ..
+                    } => {
                         input::update_from_input(keycode, false, &mut memory);
-                    },
+                    }
 
-                    Event::Window {win_event, ..} => {
+                    Event::Window { win_event, .. } => {
                         match win_event {
                             WindowEvent::Shown => {
+                                //println!("Get resolution");
                                 // there must be a better way of doing this...
                                 // if we start maximised, we can then use the
                                 // resulting client area size to discover the
                                 // maximum viewable 1024:832 window
                                 let (w, h) = canvas.output_size().unwrap();
                                 let best_w_fit = (w / 16) < (h / 13);
-                                let nw = if best_w_fit {w} else {1024 * h / 832};
-                                let nh = if best_w_fit {832 * w / 1024} else {h};
+                                let nw = if best_w_fit { w } else { 1024 * h / 832 };
+                                let nh = if best_w_fit { 832 * w / 1024 } else { h };
                                 canvas.window_mut().set_maximum_size(nw, nh).unwrap();
                                 canvas.window_mut().restore();
-                                canvas.window_mut().set_position(WindowPos::Centered, WindowPos::Centered);
+                                canvas
+                                    .window_mut()
+                                    .set_position(WindowPos::Centered, WindowPos::Centered);
                                 // leave it so as user can go bigger if they
                                 // want to - albeit with incorrect aspect ratio
                                 canvas.window_mut().set_maximum_size(w, h).unwrap();
                                 if debug {
                                     println!("Screen size {} x {}", nw, nh);
                                 }
-                            },
-                            _ => {},
+                            }
+                            _ => {}
                         }
-                    },
+                    }
 
-                    _ => {},
+                    _ => {}
                 }
             }
 
@@ -121,12 +153,12 @@ fn main() {
                 }
 
                 if memory.mapped_io.godvg != 0 {
-                    dvg.render(&mut memory, &mut canvas);
+                    dvg.render(&mut memory, &mut canvas, &mut port);
                 }
             }
             sounds.play(&memory);
 
-            memory.mapped_io.clck3khz = ((cpu.cycle / 500) & 0xFF) as u8;  
+            memory.mapped_io.clck3khz = ((cpu.cycle / 500) & 0xFF) as u8;
         }
         // sleeping at every 3khz tick is too frequent as there can still be
         // overruns on my laptop, so we even things out over a number of ticks
@@ -134,10 +166,8 @@ fn main() {
         let delta = now.elapsed();
         if delta < tick_time {
             sleep(tick_time - delta);
+        } else {
+            println!("Overrun {:?}", delta - tick_time);
         }
-        //else {
-        //    println!("Overrun {:?}", delta - tick_time);
-        //}
     }
 }
-
